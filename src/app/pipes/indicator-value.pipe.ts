abc37c0d5de5d62467e9421706386a28c7543c01
Edgar <edgarbenitez92@gmail.com>
2021-11-12 21:27:26 -0300

    Fix on bootstrap styles. Navbar incorporation. Incorporation of pipe in value column

--- src/app/pipes/indicator-value.pipe.ts
+++ src/app/pipes/indicator-value.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'indicatorValue'
+  name: 'indicatorValue',
 })
 export class IndicatorValuePipe implements PipeTransform {
+  transform(value: string, measurement: string): string {
+    if (measurement === 'Porcentaje') {
+      return value + '%';
+    } else if (measurement === 'Dólar' || measurement === 'Pesos') {
+      return '$' + value;
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    return value;
   }
-
 }
